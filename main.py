f88406fe9eacca1e51a83059b19fe62b2674dda7
Макс <maks.ruslan.2018@gmail.com>
2024-11-28 17:39:45 +0300

    Исправление TODO #2

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
     Функция для вывода
     :return:
     """
-    # TODO Миллион раз говорил, что так делать нельзя
-    pprint((get_points('uuee', 'urml')), compact=True, width=105)
+    result = get_points('uuee', 'urml')
+    pprint(result, compact=True, width=105)
 
 
 if __name__ == '__main__':
